5df71e95298f5642e56d0f1e15d7596a89ac0419
py <1256318541@qq.com>
2024-01-30 16:38:17 +0800

    feat: 播放顺序设置、播放按钮重构、自动连播

--- src/views/playListDetail/index.vue
+++ src/views/playListDetail/index.vue
@@ -39,6 +39,7 @@
             size="large"
             round
             strong
+            @click="playAll"
           >
             <n-icon size="25"> <CaretForwardCircleOutline /> </n-icon
             >&nbsp;播放全部
@@ -98,9 +99,83 @@
         position: fixed;
         bottom: 0;
         border-radius: 5%;
+        display: flex;
+        align-items: center;
       "
     >
-      <audio controls autoplay :src="songUrl"></audio>
+      <n-button
+        size="large"
+        circle
+        strong
+        quaternary
+        style="margin-left: 20px; width: 36px; height: 36px"
+        @click=""
+      >
+        <n-icon size="22"> <PlaySkipBack /></n-icon
+      ></n-button>
+      <n-button
+        size="large"
+        circle
+        strong
+        quaternary
+        style="
+          margin-left: 20px;
+          border: 1.5px solid black;
+          width: 36px;
+          height: 36px;
+        "
+        @click="musicStart"
+      >
+        <n-icon size="22">
+          <Pause v-if="isPlay" />
+          <Play v-else /> </n-icon
+      ></n-button>
+      <n-button
+        size="large"
+        circle
+        strong
+        quaternary
+        style="margin-left: 20px; width: 36px; height: 36px"
+        @click=""
+      >
+        <n-icon size="22"> <PlaySkipForward /></n-icon
+      ></n-button>
+
+      <audio
+        ref="audioRef"
+        controls
+        autoplay
+        :src="songUrl"
+        @ended="musicEnded"
+      ></audio>
+      <n-button
+        size="large"
+        circle
+        strong
+        quaternary
+        style="
+          margin-left: 20px;
+          border: 1px dashed black;
+          width: 28px;
+          height: 28px;
+        "
+        @click="
+          playType =
+            playType === 'radom'
+              ? 'list'
+              : playType === 'list'
+              ? 'cycle'
+              : playType === 'cycle'
+              ? 'radom'
+              : 'cycle'
+        "
+      >
+        <n-icon size="15">
+          <ShuffleOutline v-if="playType === 'radom'" />
+          <List v-if="playType === 'list'" />
+          <Repeat v-if="playType === 'cycle'" />
+        </n-icon>
+      </n-button>
     </div>
   </div>
 </template>
@@ -117,11 +192,23 @@ import {
   HeartOutline,
   ShareOutline,
   LogoTwitch,
+  ShuffleOutline,
+  List,
+  Repeat,
+  Play,
+  Pause,
+  PlaySkipForward,
+  PlaySkipBack,
 } from "@vicons/ionicons5";
 
 const { currentRoute } = useRouter();
 const musicList = ref<Array<Tracks>>([]);
 const songUrl = ref<string>("");
+// 播放方式
+const playType = ref<string>("radom");
+const currentIndex = ref<number>(0);
+const isPlay = ref<boolean>(false);
+const audioRef = ref();
 const neededInfo = ref<{
   name: string;
   description: string;
@@ -154,6 +241,53 @@ const clickMusic = (id: number) => {
   });
 };
 
+// 播放全部 从第一首开始播放
+const playAll = () => {
+  currentIndex.value = 0;
+  getSongUrl(musicList.value[0].id).then((res) => {
+    songUrl.value = res.data.data[0].url;
+  });
+};
+
+const musicEnded = () => {
+  switch (playType.value) {
+    case "radom":
+      getSongUrl(
+        musicList.value[Math.floor(Math.random() * musicList.value.length)].id
+      ).then((res) => {
+        songUrl.value = res.data.data[0].url;
+      });
+      break;
+    case "list":
+      currentIndex.value += 1;
+      getSongUrl(musicList.value[currentIndex.value].id).then((res) => {
+        songUrl.value = res.data.data[0].url;
+      });
+      break;
+    case "cycle":
+      getSongUrl(musicList.value[currentIndex.value].id).then((res) => {
+        songUrl.value = res.data.data[0].url;
+      });
+      break;
+
+    default:
+      break;
+  }
+};
+
+const musicStart = () => {
+  isPlay.value = !isPlay.value;
+  if (isPlay.value) {
+    if (songUrl.value !== "") {
+      audioRef.value.play();
+    } else {
+      musicEnded();
+    }
+  } else {
+    audioRef.value.pause();
+  }
+};
+
 getPlayDetail();
 </script>
 
@@ -161,7 +295,9 @@ getPlayDetail();
 &::-webkit-media-controls-enclosure {
   background-color: transparent;
 }
-
+audio::-webkit-media-controls-play-button {
+  display: none;
+}
 audio {
   width: 40%;
   height: 45px;
